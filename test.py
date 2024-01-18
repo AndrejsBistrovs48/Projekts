abbe93da2ad96d1512b9caf3f1b7b3affa50f0ed
AndrejsBistrovs48 <144618270+AndrejsBistrovs48@users.noreply.github.com>
2024-01-18 02:13:54 +0000

    webpage is saved as pdfff

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 import time
+import keyboard
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 
 
+
 service = Service()
-profile_path = r'C:\Users\drjuh\AppData\Local\Google\Chrome\User Data'  
+profile_path = r''  #private information
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f'--user-data-dir={profile_path}')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 #login ortus
-login=""
-password=""
+login=""#private information
+password=""#private information
 driver.get('https://id2.rtu.lv/')
-time.sleep(2)
 login_f=driver.find_element(By.NAME,"IDToken1")
 login_f.click()
 login_f.clear()
@@ -23,7 +24,6 @@ password_f=driver.find_element(By.ID,"IDToken2")
 password_f.send_keys(password)
 login_f=driver.find_element(By.NAME,"Login.Submit")
 login_f.click()
-time.sleep(3)
 #MAIL block
 driver.get('https://mail.google.com/mail')
 #parent=driver.getWindowHandle()
@@ -31,7 +31,7 @@ driver.get('https://mail.google.com/mail')
 
 
 
-time.sleep(2)
+time.sleep(1)
 
 search_input = driver.find_element(By.NAME, "q")
 
@@ -41,15 +41,21 @@ search_input.send_keys("pievienojis atsauksmi")
 
 search_input.send_keys(webdriver.Keys.RETURN)
 
-time.sleep(4)
+time.sleep(3)
 email_open= driver.find_element(By.CLASS_NAME, 'av')
 email_open.click()
 time.sleep(1)
 link_open= driver.find_element(By.LINK_TEXT,'uzdevuma iesniegumam')
 link_open.click()
-time.sleep(30)
+time.sleep(5)
+keyboard.press(['ctrl','p'])
+keyboard.release('ctrl')
+keyboard.release('p')
+time.sleep(3) 
+keyboard.press_and_release('enter')
+time.sleep(3)
+keyboard.press_and_release('enter')
+time.sleep(10)
 
-
-password.click()
 
 driver.quit()
